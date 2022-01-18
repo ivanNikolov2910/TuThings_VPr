11dab86d1bc5dbc7346092d46e3c710f9baaf11d
Ivan <ivan.nikolov2910@gmail.com>
2022-01-18 21:58:52 +0200

    json work 2

---
+++ vpr_control_test/test_test2.py
@@ -0,0 +1,61 @@
+import json
+
+
+class GSM_Mobile_Devices:
+    def __init__(self, quantity, price, year, make, model):
+        self.quantity = quantity
+        self.price = price
+        self.year = year
+        self.make = make
+        self.model = model
+
+    def __str__(self):
+        return f"{self.make} model {self.model} price {self.price} and {self.quantity} quantity"
+
+
+gsm = [GSM_Mobile_Devices(45, 100, 2010, 'Nokia', '3310'),
+       GSM_Mobile_Devices(20, 400, 2012, 'iPhone', '11 Pro'),
+       GSM_Mobile_Devices(30, 150, 2015, 'Nokia', '5310'),
+       GSM_Mobile_Devices(15, 200, 2021, 'Nokia', "6300")]
+
+
+def sort_gsm(gsm_list):
+    gsm_list.sort(key=lambda x: x.quantity)
+
+    for obj in gsm:
+        print(obj)
+
+
+def find_model(make, gsn_list):
+    same_make = []
+    for obj in gsn_list:
+        if obj.make == make:
+            same_make.append(obj)
+
+    f = open("file_phones.json", "a")
+    json_data = list()
+    for obj in same_make:
+        json_data.append({"Make": obj.make, "Model": obj.model, "price": obj.price, "quantity": obj.quantity,
+                          "year": obj.year})
+    f.write(json.dumps(json_data))
+
+    f.close()
+
+    # f = open("file_phones.json", "a")  # w- всеки път ти ПРЕзаписва файла, използвай "a"
+    # f.write(json.dumps(same_make))
+    # f.close()
+    # json_string=json.dumps(same_make)
+    # return json_string
+
+
+print('\n---sorted---')
+#sort_gsm(gsm)
+
+print('\n---same manufacturer---')
+find_model('Nokia', gsm)
+
+
+f = open("file_phones.json", "r")
+raw_data = f.read()
+gsm_list = json.loads(raw_data)
+print()
